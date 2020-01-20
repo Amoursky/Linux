07f075ef1dc195cede3b6a62e413f12499a1350d
Amoursky <1161079681@qq.com>
2020-01-20 18:40:47 +0800

    提交

--- test_1_15/test.c
+++ test_1_15/test.c
@@ -2,11 +2,12 @@
 #include<unistd.h>
 int main()
 {
-  for(int i = 0; i < 2; i++)
-  {
-    fork();
-    printf("=");
-    fflush(stdout);
-  }
-  return 0;
+  //for(int i = 0; i < 2; i++)
+  //{
+  //  fork();
+  //  printf("=");
+  //  fflush(stdout);
+  //}
+  printf("hehe\n");
+  return -1;
 }
