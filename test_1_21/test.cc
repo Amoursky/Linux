131f92c302a3e70a428b963bf6241ac1389dea0b
Amoursky <1161079681@qq.com>
2020-01-21 23:06:46 +0800

    提交

---
+++ test_1_21/test.cc
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+int main()
+{
+  pid_t ret = fork();
+  if(ret < 0)
+  {
+    perror("fork");
+    return 0;
+  }
+  if(ret == 0)
+  {
+    //child
+    exit(0);
+  }
+  //int status = 0;
+  ret = fork();
+  if(ret == 0)
+  {
+    exit(0);
+  }
+  wait(NULL);
+  while(1)
+  {
+    sleep(1);
+  }
+  return 0;
+}
