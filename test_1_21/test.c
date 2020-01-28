d4dbafef786ffbb034a146520305432e85226604
Amoursky <1161079681@qq.com>
2020-01-28 18:56:11 +0800

    提交

--- test_1_21/test.c
+++ test_1_21/test.c
@@ -1,45 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<stdlib.h>
-#include<sys/wait.h>
 int main()
 {
-  pid_t ret1 = fork();
-  if(ret1 == 0)
+  int i = 0;
+  for(i = 0; i < 2; i++)
   {
-    //child
-    printf("child1 %d\n",getpid());
-    sleep(3);
-    exit(0);
-  }
-  //int status = 0;
-  pid_t ret2 = fork();
-  if(ret2 == 0)
-  {
-    printf("child2 %d\n",getpid());
-    sleep(1);
-    exit(0);
-  }
-  printf("father %d\n",getpid());
-  int ret = 0;
-  int count = 0;
-  while(1)
-  {
-    ret = waitpid(-1, NULL, WNOHANG);
-    printf("wait1 %d\n",ret); 
-    if(ret > 0)
-    {
-      break;
-    }
-    count++;
-  }
-  printf("count = %d\n",count);
-  ret = waitpid(ret2, NULL, 0);
-  printf("wait2 %d\n",ret);
-  while(1)
-  {
-    sleep(1);
+    fork();
+    printf("=");
   }
   return 0;
-
 }
