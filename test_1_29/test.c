85021b95195ebc213c5115ac2a21e07978717e59
Amoursky <1161079681@qq.com>
2020-01-29 14:38:43 +0800

    提交

--- test_1_29/test.c
+++ test_1_29/test.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-  execl("/usr/bin/ls","/usr/bin/ls","/",NULL);
-
+  //最后一个参数必须是NULL，如果不填，程序就是未定义行为
+  printf("before execl\n");
+  int ret = execl("/usr/bin/ls","/usr/bin/ls","/",NULL);
+  printf("after execl %d\n",ret);
   return 0;
 }
