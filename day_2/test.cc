e8b0cb2d0acd04a438ac93a032f8232500c9d92d
Amoursky <1161079681@qq.com>
2020-01-31 12:31:42 +0800

    提交

---
+++ day_2/test.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 0;
+    // a = 'A' + 1.6;
+    // cout << a << endl;
+    int x = 10, y = 10;
+    x = y = ++y;
+    cout << x << " " << y << endl;
+    return 0;
+}
