9c8d4f42314351ca14c16ecbd6f7dd07b7ccd51c
Amoursky <1161079681@qq.com>
2020-02-24 23:00:26 +0800

    提交

--- test_2_23/test.c
+++ test_2_23/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 // class Test
 // {
 //     void func()
@@ -23,7 +24,25 @@
 
 int main()
 {
-    int fd = open("./test.txt",01);
-    printf("fd = %d\n", fd);
+    //open的返回值叫做文件描述符
+    // printf("%d\n",stdin->_fileno);
+    // printf("%d\n",stdout->_fileno);
+    // printf("%d\n",stderr->_fileno);
+    int fd = open("./test.txt",O_WRONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        return 1;
+    }
+    // 读文件
+    // char buf[1024] = {0};
+    // ssize_t n = read(fd, buf, sizeof(buf) - 1);
+    // buf[n] = '\0';
+    // printf("%s\n", buf);
+
+    // 写文件
+    char buf[1024] = "hehe";
+    ssize_t n = write(fd, buf, strlen(buf));
+    close(fd);
     return 0;
 }
