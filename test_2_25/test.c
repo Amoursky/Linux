e8d6672bb419576d3e3e0ce6d8beec5a9c2a6a7a
Amoursky <1161079681@qq.com>
2020-02-25 13:23:36 +0800

    提交

--- test_2_25/test.c
+++ test_2_25/test.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int main()
 {
-    //close(1);
-    printf("%d\n",stdout->_fileno);
+    close(1);
+    int fd = open("test.txt", O_WRONLY);
+    fprintf(stderr, "fd = %d\n", fd);
+    //printf("%d\n",stdout->_fileno); 
     return 0;
 }
