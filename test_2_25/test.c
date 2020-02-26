e2dc325ab1223c8a583799d39e2e43fbf38ed4b0
Amoursky <1161079681@qq.com>
2020-02-26 22:51:44 +0800

    提交

--- test_2_25/test.c
+++ test_2_25/test.c
@@ -7,6 +7,6 @@ int main()
     close(1);
     int fd = open("test.txt", O_WRONLY);
     fprintf(stderr, "fd = %d\n", fd);
-    //printf("%d\n",stdout->_fileno); 
+    printf("新的输出 %d\n",stdout->_fileno); 
     return 0;
 }
