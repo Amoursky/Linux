3b2180c954f9a512c6a592a8480566088d238872
Amoursky <1161079681@qq.com>
2020-03-01 12:35:44 +0800

    提交

---
+++ signal/test.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+//kill
+int main()
+{
+    // C 语言字符串转整数
+    // 1. atoi
+    // char str[] = "100";
+    // int num = atoi(str);
+    // printf("%d\n",num);
+
+    //2. sprintf/sscanf
+    // char str[] = "100";
+    // int num = 0;
+    // sscanf(str, "%d", &num);
+    // printf("%d\n",num);
+
+    
+    return 0;
+}
